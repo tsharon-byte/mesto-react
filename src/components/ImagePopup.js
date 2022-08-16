7ed563bc39fb329673a025ced414eae8080abec0
Tatiana Churkina <tatiana.churkina@stacksports.com>
2022-08-16 16:51:46 +0300

    ПР10: ревью 1

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -2,8 +2,8 @@ const ImagePopup = ({card, onClose}) => (
     <div className={`popup popup_large ${card.name && "popup popup_opened popup_large" }`} id="placeViewerPopup">
         <div className="popup__container popup__container_size_large">
             <figure className="figure">
-                <img className="popup__image" src={card.link&&card.link} alt={card.name&&card.name}/>
-                <figcaption className="figure__caption">{card.name&&card.name}</figcaption>
+                <img className="popup__image" src={card.link} alt={card.name}/>
+                <figcaption className="figure__caption">{card.name}</figcaption>
             </figure>
             <button className="image-button popup__close-icon" type="button" onClick={onClose}/>
         </div>
